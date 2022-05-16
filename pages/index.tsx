6bfdf953be480a896f705f6dc42f13c906d9ecae
elloot <54401777+elloot@users.noreply.github.com>
2022-05-16 22:41:22 +0200

    🚧 start work on generating a game board

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,56 @@
+import { Tile } from '../components/Tile';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-  return <div>Smack my ass like a drum</div>;
+  const width = 30;
+  const height = 16;
+  //const board: number[] = Array(width * height);
+  const [board, setBoard] = useState<number[]>(Array(width * height));
+
+  function coordsToIndex(x: number, y: number) {
+    return y * width + x;
+  }
+
+  useEffect(() => {
+    let indices: number[] = [];
+    for (let i = 0; i < width * height; i++) {
+      indices.push(i);
+    }
+
+    let tempBoard: number[] = Array(width * height);
+
+    tempBoard.fill(0);
+
+    for (let i = 0; i < 99; i++) {
+      const tileIndex = indices[Math.floor(Math.random() * indices.length)];
+      tempBoard[tileIndex] = 1;
+      indices.splice(indices.indexOf(tileIndex), 1);
+    }
+
+    setBoard(tempBoard);
+
+    console.log(tempBoard.filter((value) => value == 1).length);
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: 300 + 'px',
+        height: 'auto',
+        display: 'flex',
+        flexWrap: 'wrap'
+      }}
+    >
+      {board.map((value, index) => (
+        <Tile
+          width={10}
+          height={10}
+          handleClick={() => null}
+          value={value}
+          index={index}
+          key={index}
+        />
+      ))}
+    </div>
+  );
 }
