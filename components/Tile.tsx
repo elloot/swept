d35399bbbb618c8400339bff6d7fb9aa32bbbd29
elloot <54401777+elloot@users.noreply.github.com>
2022-05-16 22:41:08 +0200

    ✨ add Tile component

---
+++ components/Tile.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+interface TileProps {
+  width: number;
+  height: number;
+  handleClick: (index: number) => void;
+  index: number;
+  value: number;
+}
+
+export const Tile: React.FC<TileProps> = ({
+  width,
+  height,
+  handleClick,
+  index,
+  value
+}) => {
+  return (
+    <div style={{ width: width + 'px', height: height + 'px' }}>{value}</div>
+  );
+};
