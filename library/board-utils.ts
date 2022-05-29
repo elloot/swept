50fb9cc68714d932bebd7ada56eb64659af9d929
elloot <54401777+elloot@users.noreply.github.com>
2022-05-29 17:38:06 +0200

    ♻️ extract board utility functions

---
+++ library/board-utils.ts
@@ -0,0 +1,101 @@
+function coordsToIndex(x: number, y: number, width: number): number {
+  return y * width + x;
+}
+
+function indexToCoords(index: number, width: number): { x: number; y: number } {
+  return { x: index % width, y: Math.floor(index / width) };
+}
+
+function fillBoardWithMineCounts(
+  mineBoard: number[],
+  boardWidth: number,
+  boardHeight: number
+) {
+  for (let tileIndex = 0; tileIndex < mineBoard.length; tileIndex++) {
+    const tileValue = mineBoard[tileIndex];
+    if (tileValue === 0) {
+      const mineCount = countMinesNearTile(
+        tileIndex,
+        mineBoard,
+        boardWidth,
+        boardHeight
+      );
+      mineBoard[tileIndex] = mineCount;
+    }
+  }
+}
+
+function countMinesNearTile(
+  tileIndex: number,
+  tileBoard: number[],
+  boardWidth: number,
+  boardHeight
+): number {
+  const tilePos = indexToCoords(tileIndex, boardWidth);
+  let mineCount = 0;
+  iterateOverClosestTiles(tilePos, boardWidth, boardHeight, (closeTile) => {
+    if (tileBoard[coordsToIndex(closeTile.x, closeTile.y, boardWidth)] == -1) {
+      mineCount++;
+    }
+  });
+  return mineCount;
+}
+
+function generateBoardWithMines(
+  width: number,
+  height: number,
+  firstTileIndex: number
+): number[] {
+  const tempBoard: number[] = Array(width * height);
+  tempBoard.fill(0);
+  const indices = generateIndices(width, height, firstTileIndex);
+
+  for (let i = 0; i < 99; i++) {
+    const tileIndex = indices[Math.floor(Math.random() * indices.length)];
+    tempBoard[tileIndex] = -1;
+    indices.splice(indices.indexOf(tileIndex), 1);
+  }
+
+  return tempBoard;
+}
+
+function generateIndices(
+  width: number,
+  height: number,
+  firstTileIndex: number
+): number[] {
+  let indices: number[] = [];
+  for (let i = 0; i < width * height; i++) {
+    indices.push(i);
+  }
+  indices.splice(indices.indexOf(firstTileIndex), 1);
+  return indices;
+}
+
+function iterateOverClosestTiles(
+  centerTilePos: { x: number; y: number },
+  boardWidth: number,
+  boardHeight: number,
+  callback: (closeTile: { x: number; y: number }) => void
+) {
+  for (let y = -1; y <= 1; y++) {
+    for (let x = -1; x <= 1; x++) {
+      if (
+        centerTilePos.x + x >= 0 &&
+        centerTilePos.x + x < boardWidth &&
+        centerTilePos.y + y >= 0 &&
+        centerTilePos.y + y < boardHeight
+      ) {
+        callback({ x: centerTilePos.x + x, y: centerTilePos.y + y });
+      }
+    }
+  }
+}
+
+export {
+  coordsToIndex,
+  indexToCoords,
+  generateBoardWithMines,
+  fillBoardWithMineCounts,
+  iterateOverClosestTiles
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,79 @@
 import { Tile } from '../components/Tile';
 import { createRef, useCallback, useEffect, useMemo, useState } from 'react';
+import {
+  generateBoardWithMines,
+  fillBoardWithMineCounts,
+  indexToCoords,
+  coordsToIndex,
+  iterateOverClosestTiles
+} from '../library/board-utils';
 
 export default function Home() {
   const [hasRun, setHasRun] = useState<boolean>(false);
   const [indexOfFirstClickedTile, setIndexOfFirstClickedTile] =
     useState<number>();
-  const width = 30;
-  const height = 16;
-  const [board, setBoard] = useState<number[]>(Array(width * height).fill(1));
+  const boardWidth = 30;
+  const boardHeight = 16;
+  const [board, setBoard] = useState<number[]>(
+    Array(boardWidth * boardHeight).fill(1)
+  );
   const tiles: React.RefObject<Tile>[] = [];
 
-  function coordsToIndex(x: number, y: number) {
-    return y * width + x;
-  }
-
-  function indexToCoords(index: number): { x: number; y: number } {
-    return { x: index % width, y: Math.floor(index / width) };
-  }
-
-  function clickHandler(index: number): void {
+  function clickHandler(tileIndex: number): void {
     if (!hasRun) {
       setHasRun(true);
-      setBoard(generateBoard(index));
-      setIndexOfFirstClickedTile(index);
+      setBoard(generateBoard(tileIndex));
+      setIndexOfFirstClickedTile(tileIndex);
     }
 
-    if (board[index] === 0) {
-      const visitedZeroTiles = [index];
-      revealClosestTiles(index, visitedZeroTiles);
+    if (board[tileIndex] === 0) {
+      const visitedZeroTiles = [tileIndex];
+      revealClosestTiles(tileIndex, visitedZeroTiles);
     }
   }
 
   const revealClosestTiles = useCallback(
-    (index, visitedZeroTiles) => {
+    (index: number, visitedZeroTiles: number[]) => {
       const tileValue = board[index];
       if (tileValue === 0) {
-        const tilePos = indexToCoords(index);
-        iterateOverClosestTiles(tilePos, (closeTile) => {
-          const closeTileIndex = coordsToIndex(closeTile.x, closeTile.y);
-          if (
-            board[closeTileIndex] === 0 &&
-            !visitedZeroTiles.includes(closeTileIndex)
-          ) {
-            visitedZeroTiles.push(closeTileIndex);
-            revealClosestTiles(closeTileIndex, visitedZeroTiles);
-          }
+        const tilePos = indexToCoords(index, boardWidth);
+        iterateOverClosestTiles(
+          tilePos,
+          boardWidth,
+          boardHeight,
+          (closeTile) => {
+            const closeTileIndex = coordsToIndex(
+              closeTile.x,
+              closeTile.y,
+              boardWidth
+            );
+            if (
+              board[closeTileIndex] === 0 &&
+              !visitedZeroTiles.includes(closeTileIndex)
+            ) {
+              visitedZeroTiles.push(closeTileIndex);
+              revealClosestTiles(closeTileIndex, visitedZeroTiles);
+            }
 
-          tiles[closeTileIndex].current.setFace('CLEAR');
-        });
+            tiles[closeTileIndex].current.setFace('CLEAR');
+          }
+        );
       }
     },
     [board, tiles]
   );
+
+  function generateBoard(firstTileIndex: number) {
+    const tempBoard = generateBoardWithMines(
+      boardWidth,
+      boardHeight,
+      firstTileIndex
+    );
+
+    fillBoardWithMineCounts(tempBoard, boardWidth, boardHeight);
+
+    return tempBoard;
+  }
 
   useEffect(() => {
     if (board[indexOfFirstClickedTile] === 0) {
@@ -59,59 +81,6 @@ export default function Home() {
       revealClosestTiles(indexOfFirstClickedTile, visitedZeroTiles);
     }
   }, [indexOfFirstClickedTile, board, revealClosestTiles]);
-
-  function generateBoard(firstTileIndex: number) {
-    let indices: number[] = [];
-    for (let i = 0; i < width * height; i++) {
-      indices.push(i);
-    }
-
-    indices.splice(indices.indexOf(firstTileIndex), 1);
-
-    let tempBoard: number[] = Array(width * height);
-
-    tempBoard.fill(0);
-
-    for (let i = 0; i < 99; i++) {
-      const tileIndex = indices[Math.floor(Math.random() * indices.length)];
-      tempBoard[tileIndex] = -1;
-      indices.splice(indices.indexOf(tileIndex), 1);
-    }
-
-    for (let i = 0; i < tempBoard.length; i++) {
-      const tileValue = tempBoard[i];
-      let mineCount = 0;
-      if (tileValue === 0) {
-        const tilePos = indexToCoords(i);
-        iterateOverClosestTiles(tilePos, (closeTile) => {
-          if (tempBoard[coordsToIndex(closeTile.x, closeTile.y)] == -1) {
-            mineCount++;
-          }
-        });
-        tempBoard[i] = mineCount;
-      }
-    }
-
-    return tempBoard;
-  }
-
-  function iterateOverClosestTiles(
-    centerTilePos: { x: number; y: number },
-    callback: (closeTile: { x: number; y: number }) => void
-  ) {
-    for (let y = -1; y <= 1; y++) {
-      for (let x = -1; x <= 1; x++) {
-        if (
-          centerTilePos.x + x >= 0 &&
-          centerTilePos.x + x < width &&
-          centerTilePos.y + y >= 0 &&
-          centerTilePos.y + y < height
-        ) {
-          callback({ x: centerTilePos.x + x, y: centerTilePos.y + y });
-        }
-      }
-    }
-  }
 
   return (
     <div
